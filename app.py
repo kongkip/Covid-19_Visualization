0232d04fbbe2945d9e43ad42bc15cd9a3da56987
kongkip <evanskiplagat3@gmail.com>
2020-03-17 00:09:02 +0300

    updated app

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ server = flask.Flask(__name__)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, server=server)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, server=server, title="Covid-19 Reports")
+
+# app.title = "Covid-19 Reports"
 
 colors = {
     'background': '#ffffff',
