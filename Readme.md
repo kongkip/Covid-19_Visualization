1a9736a76c58687776cf63aff5ce6d65195fadd5
kongkip <evanskiplagat3@gmail.com>
2020-03-16 23:43:37 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,23 @@
+# Covid-19 data dashboard app
+[WHO explains](https://www.who.int/health-topics/coronavirus)
+
+Coronaviruses (CoV) are a large family of viruses that cause illness ranging from the common cold to more severe diseases such as Middle East Respiratory Syndrome (MERS-CoV) and Severe Acute Respiratory Syndrome (SARS-CoV).
+
+Coronavirus disease (COVID-19) is a new strain that was discovered in 2019 and has not been previously identified in humans.
+
+##### Common signs of infection include respiratory symptoms:
+* Fever
+* Cough
+* Shortness of Breath, and
+* Breathing Deficulties
+
+##### In severe cases can cause:
+* pneumonia
+* severe acute respirtory syndrome
+* Kidney failure, and
+* Death
+
+# About the App
+This is an app build with dash, it explains to users what Covid-19 is,
+users are able to select country of origin to update the data dynamically
+also reports the current stats e.g death and recovery rate
---
+++ get_data.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+import numpy as np
+import seaborn as sns
+
+confirmed = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
+deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
+recovered = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv")
+
+
+# create a time series data
+def get_coutries():
+    return confirmed["Country/Region"].unique()
+
+def prepare_data(case):
+    """
+    case:
+        pandas data frame for either cofirmed/deaths/recovered
+    """
+    # select only the columns with time
+    case_T = case[case.columns[-54:]].T 
+
+     # set columns names to Country/Region
+    case_T.columns = case["Country/Region"]
+
+    # transform index to datetime
+    case_T.index = pd.to_datetime(case_T.index) 
+
+    # sum data of regions in the same country
+    regions_count = pd.value_counts(confirmed["Country/Region"]).reset_index()
+    regions_count.columns = ["Country/Region","Counts"]
+    to_drop = list(regions_count[regions_count["Counts"]>1]["Country/Region"].values)
+    case_T_copy = case_T.copy()
+    case_T_copy = case_T_copy.drop(to_drop, axis=1)
+    for i in to_drop:
+        case_T_copy[i] = case_T[i].sum(axis=1)
+    
+    return case_T_copy
+
+#  filter data by country
+def filter_by_country(country, confirmed, deaths, recovered):
+    confirmed_prepared = prepare_data(confirmed)
+    deaths_prepared = prepare_data(deaths)
+    recovered_prepared = prepare_data(recovered)
+    country_all  = pd.DataFrame(
+        data=
+            confirmed_prepared[country].values,
+        columns = [
+            "Confirmed",
+        ],
+        index=confirmed_prepared.index
+    )
+
+    country_all["Deaths"] = deaths_prepared[country].values
+    country_all["Recovered"] = recovered_prepared[country].values
+    country_all.index = pd.to_datetime(country_all.index)
+
+    return country_all
