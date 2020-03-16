2fc32da1416d53c557749f5ef83b72f115cdab9e
kongkip <evanskiplagat3@gmail.com>
2020-03-16 23:49:10 +0300

    minor fixes

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-
 import pandas as pd
 import numpy as np
 import dash
-import dash_core_components as dcc 
-import dash_html_components as html 
+import dash_core_components as dcc
+import dash_html_components as html
 from dash.dependencies import Input, Output
 from get_data import filter_by_country, get_coutries
 
@@ -16,17 +15,20 @@ colors = {
     'text': '#111111'
 }
 
+confirmed = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data"
+                        "/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
+deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data"
+                     "/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
+recovered = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data"
+                        "/csse_covid_19_time_series/time_series_19-covid-Recovered.csv")
 
-confirmed = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
-deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
-recovered = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv")
-
-countries =  confirmed["Country/Region"].unique()
+countries = confirmed["Country/Region"].unique()
 
 covid_text = """
 [WHO explains](https://www.who.int/health-topics/coronavirus)
 
-Coronaviruses (CoV) are a large family of viruses that cause illness ranging from the common cold to more severe diseases such as Middle East Respiratory Syndrome (MERS-CoV) and Severe Acute Respiratory Syndrome (SARS-CoV).
+Coronaviruses (CoV) are a large family of viruses that cause illness ranging from the common cold to more severe 
+diseases such as Middle East Respiratory Syndrome (MERS-CoV) and Severe Acute Respiratory Syndrome (SARS-CoV). 
 
 Coronavirus disease (COVID-19) is a new strain that was discovered in 2019 and has not been previously identified in humans.
 
@@ -47,72 +49,73 @@ Coronavirus disease (COVID-19) is a new strain that was discovered in 2019 and h
 * Select in the dropdown menu below the country of interest; included inside - Cruise Ship
 """
 
-acklowledge = """
+acknowledge = """
 ## Acknowledgement:
 
 * Data from [John Hopkins University Repo](https://github.com/CSSEGISandData/COVID-19)
 * [WHO](https://www.who.int/)
 """
 
-#%%
+# %%
 app.layout = html.Div(
     style={'backgroundColor': colors['background']},
-    children =[
+    children=[
         html.H1(children='Covid-19 World Wide reported cases', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-    #     html.Div(children='Covid-19 cases.', style={
-    #     'textAlign': 'center',
-    #     'color': colors['text']
-    # }),
-    dcc.Markdown(children=
-        covid_text,
-        style ={
-            "margin-top":40,
-            "margin-left":50,
-            "margin-right":30}
+            'textAlign': 'center',
+            'color': colors['text']
+        }),
+        #     html.Div(children='Covid-19 cases.', style={
+        #     'textAlign': 'center',
+        #     'color': colors['text']
+        # }),
+        dcc.Markdown(children=
+                     covid_text,
+                     style={
+                         "margin-top": 40,
+                         "margin-left": 50,
+                         "margin-right": 30}
 
-    ),
-    dcc.Dropdown(
-        id = "country",
-        options=[
-            {'label': i, 'value': i} for i in countries
-        ],
-        # value=['Thailand'],
-        multi=False,
-        style ={
-            "margin-top":40,
-            "margin-left":20,
-            "margin-right":50}
-    ),
-    # dcc.Markdown(children=covid_text
+                     ),
+        dcc.Dropdown(
+            id="country",
+            options=[
+                {'label': i, 'value': i} for i in countries
+            ],
+            # value=['Thailand'],
+            multi=False,
+            style={
+                "margin-top": 40,
+                "margin-left": 20,
+                "margin-right": 50}
+        ),
+        # dcc.Markdown(children=covid_text
 
-    # ),
+        # ),
 
-    dcc.Graph(
-        id='cases-data',
-    
-    ),
+        dcc.Graph(
+            id='cases-data',
 
-    dcc.Markdown(
-        id = "explanation",
-        # children = """Explanations"""
-        style ={
-            "margin-top":60,
-            "margin-left":50,
-            "margin-right":50,
-            "color": "#0000FF"}
-    ),
+        ),
 
-    dcc.Markdown(
-        children = acklowledge,
-        style ={
-            "margin-top":60,
-            "margin-left":50,
-            "margin-right":50}
-    )
-])
+        dcc.Markdown(
+            id="explanation",
+            # children = """Explanations"""
+            style={
+                "margin-top": 60,
+                "margin-left": 50,
+                "margin-right": 50,
+                "color": "#0000FF"}
+        ),
+
+        dcc.Markdown(
+            children=acknowledge,
+            style={
+                "margin-top": 60,
+                "margin-left": 50,
+                "margin-right": 50}
+        )
+    ])
+
 
 @app.callback(
     Output("cases-data", "figure"),
@@ -120,57 +123,58 @@ app.layout = html.Div(
 )
 def update_country(country):
     df = filter_by_country(country, confirmed, deaths, recovered)
-    return  {
-    'data': [
-        dict(
-            x=df.index,
-            y=df["Confirmed"],
-            mode='markers',
-            opacity=0.7,
-            marker={
-                'size': 15,
-                'line': {'width': 0.5, 'color': 'white'}
+    return {
+        'data': [
+            dict(
+                x=df.index,
+                y=df["Confirmed"],
+                mode='markers',
+                opacity=0.7,
+                marker={
+                    'size': 15,
+                    'line': {'width': 0.5, 'color': 'white'}
+                },
+                name="Confirmed"
+            ),
+            dict(
+                x=df.index,
+                y=df["Deaths"],
+                mode='markers',
+                opacity=0.7,
+                marker={
+                    'size': 15,
+                    'line': {'width': 0.5, 'color': 'white'}
+                },
+                name="Deaths"
+            ),
+            dict(
+                x=df.index,
+                y=df["Recovered"],
+                mode='markers',
+                opacity=0.7,
+                marker={
+                    'size': 15,
+                    'line': {'width': 0.5, 'color': 'white'}
+                },
+                name="Recovered"
+            )
+        ],
+        'layout': dict(
+            xaxis={
+                'title': "Date"
             },
-            name="Confirmed"
-        ),
-        dict(
-            x=df.index,
-            y=df["Deaths"],
-            mode='markers',
-            opacity=0.7,
-            marker={
-                'size': 15,
-                'line': {'width': 0.5, 'color': 'white'}
+            yaxis={
+                'title': "Number of cases",
             },
-            name= "Deaths"
-        ),
-        dict(
-            x=df.index,
-            y=df["Recovered"],
-            mode='markers',
-            opacity=0.7,
-            marker={
-                'size': 15,
-                'line': {'width': 0.5, 'color': 'white'}
-            },
-            name = "Recovered"
-        )
-    ],
-    'layout': dict(
-        xaxis={
-            'title': "Date"
-        },
-        yaxis={
-            'title': "Number of cases",
-        },
-        title= "{} Covid-19 cases".format(country)
+            title="{} Covid-19 cases".format(country)
 
-        ,
-        margin={'l': 40, 'b': 30, 't': 60, 'r': 20},
-        height=450,
-        hovermode='closest'
-    )
-}
+            ,
+            margin={'l': 40, 'b': 30, 't': 60, 'r': 20},
+            height=450,
+            hovermode='closest'
+        )
+    }
+
 
 @app.callback(
     Output("explanation", "children"),
@@ -178,20 +182,22 @@ def update_country(country):
 )
 def explain(country):
     df = filter_by_country(country, confirmed, deaths, recovered)
-    
+
     confirmed_current = df["Confirmed"].values[-1]
     deaths_current = df["Deaths"].values[-1]
     recovered_current = df["Recovered"].values[-1]
 
-    death_rate = (deaths_current/confirmed_current)*100
-    recovery_rate = (recovered_current/confirmed_current)*100
+    death_rate = (deaths_current / confirmed_current) * 100
+    recovery_rate = (recovered_current / confirmed_current) * 100
     return u""" ## {} Current Reports: 
     
     * Current Confirmed : {}
     * Current Deaths : {}
     * Current Recoveries: {}
     * Date Rate : {}%
-    * Recovery Rate : {}%""".format(country, confirmed_current, deaths_current, recovered_current, death_rate, recovery_rate)
-    
+    * Recovery Rate : {}%""".format(country, confirmed_current, deaths_current, recovered_current, death_rate,
+                                    recovery_rate)
+
+
 if __name__ == "__main__":
     app.run_server(debug=False)
--- get_data.py
+++ get_data.py
@@ -1,16 +1,23 @@
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-confirmed = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
-deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
-recovered = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv")
+confirmed = pd.read_csv(
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series"
+    "/time_series_19-covid-Confirmed.csv")
+deaths = pd.read_csv(
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series"
+    "/time_series_19-covid-Deaths.csv")
+recovered = pd.read_csv(
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series"
+    "/time_series_19-covid-Recovered.csv")
 
 
 # create a time series data
-def get_coutries():
+def get_countries():
     return confirmed["Country/Region"].unique()
+
 
 def prepare_data(case):
     """
@@ -18,34 +25,35 @@ def prepare_data(case):
         pandas data frame for either cofirmed/deaths/recovered
     """
     # select only the columns with time
-    case_T = case[case.columns[-54:]].T 
+    case_T = case[case.columns[-54:]].T
 
-     # set columns names to Country/Region
+    # set columns names to Country/Region
     case_T.columns = case["Country/Region"]
 
     # transform index to datetime
-    case_T.index = pd.to_datetime(case_T.index) 
+    case_T.index = pd.to_datetime(case_T.index)
 
     # sum data of regions in the same country
     regions_count = pd.value_counts(confirmed["Country/Region"]).reset_index()
-    regions_count.columns = ["Country/Region","Counts"]
-    to_drop = list(regions_count[regions_count["Counts"]>1]["Country/Region"].values)
+    regions_count.columns = ["Country/Region", "Counts"]
+    to_drop = list(regions_count[regions_count["Counts"] > 1]["Country/Region"].values)
     case_T_copy = case_T.copy()
     case_T_copy = case_T_copy.drop(to_drop, axis=1)
     for i in to_drop:
         case_T_copy[i] = case_T[i].sum(axis=1)
-    
+
     return case_T_copy
+
 
 #  filter data by country
 def filter_by_country(country, confirmed, deaths, recovered):
     confirmed_prepared = prepare_data(confirmed)
     deaths_prepared = prepare_data(deaths)
     recovered_prepared = prepare_data(recovered)
-    country_all  = pd.DataFrame(
+    country_all = pd.DataFrame(
         data=
-            confirmed_prepared[country].values,
-        columns = [
+        confirmed_prepared[country].values,
+        columns=[
             "Confirmed",
         ],
         index=confirmed_prepared.index
