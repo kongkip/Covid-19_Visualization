a627bd52c7237baed495f0dc68e856fd0c84d3fd
kongkip <evanskiplagat3@gmail.com>
2020-03-17 19:48:05 +0300

    re-wrote app

--- app.py
+++ app.py
@@ -4,20 +4,31 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import dash_daq as daq
 from get_data import filter_by_country, get_countries
 import flask
+import pathlib
 
 server = flask.Flask(__name__)
 
+app = dash.Dash(
+    __name__,
+    meta_tags=[{"name": "viewport", "content": "width=device, initial-scale=1"}],
+    server=server
+)
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app.config["suppress_callback_exceptions"] = True
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, server=server)
+APP_PATH = str(pathlib.Path(__file__).parent.resolve())
+app.title = "Covid-19 Reports"
 
-colors = {
-    'background': '#ffffff',
-    'text': '#111111'
-}
+suffix_row = "_row"
+suffix_button_id = "_button"
+suffix_sparkline_graph = "_sparkline_graph"
+suffix_count = "_count"
+suffix_ooc_n = "_OOC_number"
+suffix_ooc_g = "_OOC_graph"
+suffix_indicator = "_indicator"
 
 confirmed = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data"
                         "/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
@@ -28,101 +39,124 @@ recovered = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-
 
 countries = confirmed["Country/Region"].unique()
 
-covid_text = """
-[WHO explains](https://www.who.int/health-topics/coronavirus)
 
-Coronaviruses (CoV) are a large family of viruses that cause illness ranging from the common cold to more severe 
-diseases such as Middle East Respiratory Syndrome (MERS-CoV) and Severe Acute Respiratory Syndrome (SARS-CoV). 
+def build_banner():
+    return html.Div(
+        id="banner",
+        className="banner",
+        children=[
+            html.Div(
+                id="banner-text",
+                children=[
+                    html.H5("Covid-19 cases"),
+                    html.H6("Reports and visualization dashboard")
+                ],
+            ),
+            # html.Div(
+            #     id="banner-logo",
+            #     children=[
+            #         html.Button(
+            #             id="learn-more-button", children="LEARN MORE", n_clicks=0
+            #         ),
+            #         html.Img(id="logo",src=app.get_asset_url("dash-logo-new-png"))
+            #     ],
+            # )
+        ],
+    )
 
-Coronavirus disease (COVID-19) is a new strain that was discovered in 2019 and has not been previously identified in humans.
 
-##### Common signs of infection include respiratory symptoms:
-* Fever
-* Cough
-* Shortness of Breath, and
-* Breathing Dificulties
+def build_tabs():
+    return html.Div(
+        id="tabs",
+        className="tabs",
+        children=[
+            dcc.Tabs(
+                id="app-tabs",
+                value="tab2",
+                className="custom-tabs",
+                children=[
+                    dcc.Tab(
+                        id="Specs-tab",
+                        label="Information On Covid-19 ",
+                        value="tab1",
+                        className="custom-tab",
+                        selected_className="custom-tab--selected",
+                        children=[
+                            dcc.Markdown(
+                                id="info",
+                                children=[
+                                    """
+                                    # About the Virus
+                                    [WHO explains](https://www.who.int/health-topics/coronavirus)
+                                    Coronaviruses (CoV) are a large family of viruses that cause illness ranging from the common cold to more severe 
+                                    diseases such as Middle East Respiratory Syndrome (MERS-CoV) and Severe Acute Respiratory Syndrome (SARS-CoV). 
+                                    Coronavirus disease (COVID-19) is a new strain that was discovered in 2019 and has not been previously identified in humans.
+                                    ##### Common signs of infection include respiratory symptoms:
+                                    * Fever
+                                    * Cough
+                                    * Shortness of Breath, and
+                                    * Breathing Dificulties
+                                    ##### In severe cases can cause:
+                                    * pneumonia
+                                    * severe acute respirtory syndrome
+                                    * Kidney failure, and
+                                    * Death
+                                    ## This dashboard gives you updates on reported cases
+                                    * Keep Calm and Wash your hands
 
-##### In severe cases can cause:
-* pneumonia
-* severe acute respirtory syndrome
-* Kidney failure, and
-* Death
 
-## This dashboard gives you updates on reported cases
-* Keep Calm and Wash your hands
-* Select in the dropdown menu below the country of interest; included inside - Cruise Ship
-"""
+                                    ## Acknowledgement:
+                                    * [Project Repo](https://github.com/kongkip/Covid-19_Visualization)
+                                    * Data from [John Hopkins University Repo](https://github.com/CSSEGISandData/COVID-19)
+                                    * [WHO](https://www.who.int/)
+                                    """
+                                ],
+                                style={
+                                    "margin-top": 20
+                                }
+                            )
+                        ]
+                    ),
+                    dcc.Tab(
+                        id="Control-chart-tab",
+                        label="Visualize Cases",
+                        value="tab2",
+                        className="custom-tab",
+                        selected_className="custom-tab--selected",
+                        children=[
+                            dcc.Dropdown(
+                                id="country",
+                                options=[
+                                    {'label': i, 'value': i} for i in countries
+                                ],
+                                multi=False,
+                            ),
+                            dcc.Graph(
+                                id="cases-data"
+                            ),
+                            dcc.Markdown(
+                                id="explanation",
+                            )
+                        ]
+                    ),
+                ],
+            )
+        ],
+    )
 
-acknowledge = """
-
-## Acknowledgement:
-
-* [Project Repo](https://github.com/kongkip/Covid-19_Visualization)
-* Data from [John Hopkins University Repo](https://github.com/CSSEGISandData/COVID-19)
-* [WHO](https://www.who.int/)
-"""
-
-# %%
-app.title = "Covid-19 Reports"
 
 app.layout = html.Div(
-    style={'backgroundColor': colors['background']},
+    id="big-app-container",
     children=[
-        html.H1(children='Covid-19 World Wide reported cases', style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }),
-        #     html.Div(children='Covid-19 cases.', style={
-        #     'textAlign': 'center',
-        #     'color': colors['text']
-        # }),
-        dcc.Markdown(children=
-                     covid_text,
-                     style={
-                         "margin-top": 40,
-                         "margin-left": 50,
-                         "margin-right": 30}
-
-                     ),
-        dcc.Dropdown(
-            id="country",
-            options=[
-                {'label': i, 'value': i} for i in countries
-            ],
-            # value=['Thailand'],
-            multi=False,
-            style={
-                "margin-top": 40,
-                "margin-left": 20,
-                "margin-right": 50}
-        ),
-        # dcc.Markdown(children=covid_text
-
-        # ),
-
-        dcc.Graph(
-            id='cases-data',
-
-        ),
-
-        dcc.Markdown(
-            id="explanation",
-            # children = """Explanations"""
-            style={
-                "margin-top": 60,
-                "margin-left": 50,
-                "margin-right": 50,
-                "color": colors['text']}
-        ),
-
-        dcc.Markdown(
-            children=acknowledge,
-            style={
-                "margin-top": 60,
-                "margin-left": 50,
-                "margin-right": 50}
+        build_banner(),
+        html.Div(
+            id="app-container",
+            children=[
+                build_tabs()
+            ]
         )
-    ])
+    ]
+)
 
 
 @app.callback(
@@ -197,15 +231,19 @@ def explain(country):
 
     death_rate = (deaths_current / confirmed_current) * 100
     recovery_rate = (recovered_current / confirmed_current) * 100
-    return u""" ## {} Current Reports: 
-    
+    return u""" #### {} Current Reports: 
+
     * Current Confirmed : {}
     * Current Deaths : {}
     * Current Recoveries: {}
     * Date Rate : {}%
-    * Recovery Rate : {}%""".format(country, confirmed_current, deaths_current, recovered_current, death_rate,
+    * Recovery Rate : {}%""".format(country, confirmed_current,
+                                    deaths_current,
+                                    recovered_current,
+                                    death_rate,
                                     recovery_rate)
 
 
 if __name__ == "__main__":
     app.run_server(debug=False)
+
